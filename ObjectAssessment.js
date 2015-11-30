29fb950754924b5f3e2ef617c4e4e45369b2ee0a
Mark Rothermal <rothermal2@comcast.net>
2015-11-30 02:14:50 -0600

    yo gabba gabba

--- ObjectAssessment.js
+++ ObjectAssessment.js
@@ -5,9 +5,6 @@ avengersMovie = {
 	Director : "Joss Whedon"
 };
 console.log(avengersMovie);
-
-assemble = function (){
-  
-  return document.getElementById("avengersAssemble").innerHTML = avengersMovie.name + " is a movie directed by"+avengersMovie.Director +". It was released in " +avengersMovie.releaseYear+" and it has a runtime of "+avengersMovie.runtime ;
-};
-assemble();
+window.onload = function(){
+document.getElementById("avengersAssemble").innerHTML = avengersMovie.name + " is a movie directed by "+avengersMovie.Director +". It was released in " +avengersMovie.releaseYear+" and it has a runtime of "+avengersMovie.runtime ;
+}

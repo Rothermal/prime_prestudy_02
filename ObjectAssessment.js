3208ab1040913c0fd56afab198bd9561b77cec68
Mark Rothermal <rothermal2@comcast.net>
2015-11-30 01:54:34 -0600

    Update ObjectAssessment.js

--- ObjectAssessment.js
+++ ObjectAssessment.js
@@ -5,3 +5,9 @@ avengersMovie = {
 	Director : "Joss Whedon"
 };
 console.log(avengersMovie);
+
+assemble = function (){
+  
+  return document.getElementById("avengersAssemble").innerHTML = avengersMovie.name + " is a movie directed by"+avengersMovie.Director +". It was released in " +avengersMovie.releaseYear+" and it has a runtime of "+avengersMovie.runtime ;
+};
+assemble();
